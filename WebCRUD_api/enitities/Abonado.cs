6197c91e25cf43ad3b8572af07013246f605a0b2
yecomprofessional07-hash <yecomprofessional07@gmail.com>
2026-03-03 07:24:59 -0600

    Subiendo Proyecto

---
+++ WebCRUD_api/enitities/Abonado.cs
@@ -0,0 +1,15 @@
+﻿namespace WebCRUD_api.enitities
+{
+    public class Abonado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public double Saldo { get; set; }
+        public DateTime Fechapago { get; set; } = DateTime.Now;
+        public bool Activo { get; set; } = true;
+    }
+}
